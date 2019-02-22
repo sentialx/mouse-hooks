3932b5b2d0fc909ee9a1ad9f2826cec084820a58
Sential <sentialx@gmail.com>
2019-02-22 22:44:45 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { EventEmitter } from "events";
+import { fork, ChildProcess } from "child_process";
+
+let registeredEvents: string[] = [];
+
+let mouseProcess: ChildProcess;
+
+class MouseEvents extends EventEmitter {
+  constructor() {
+    super();
+
+    this.on("newListener", event => {
+      if (registeredEvents.indexOf(event) !== -1) return;
+
+      if (event === "mouse-up" && !mouseProcess) {
+        mouseProcess = fork("./mouse.js");
+
+        mouseProcess.on("message", msg => {
+          if (msg === "mouse-up") {
+            this.emit("mouse-up");
+          }
+        });
+      } else {
+        return;
+      }
+
+      registeredEvents.push(event);
+    });
+
+    this.on("removeListener", event => {
+      if (this.listenerCount(event) > 0) return;
+
+      if (event === "mouse-up" && mouseProcess) {
+        mouseProcess.kill();
+      }
+
+      registeredEvents = registeredEvents.filter(x => x !== event);
+    });
+  }
+}
+
+const mouseEvents = new MouseEvents();
+
+export default mouseEvents;
