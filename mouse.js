498ef652d97d17593d3feb2b6f784de98e8b8cf9
Sential <sentialx@gmail.com>
2019-05-08 23:46:36 +0200

    fix: addon not resolving

--- mouse.js
+++ mouse.js
@@ -1,4 +1,4 @@
-const addon = require("bindings")("windows-mouse-hooks");
+const addon = require("bindings")("addon");
 
 addon.createMouseHook((event, x, y, which) => {
   process.send({ event, x, y, button: which });
