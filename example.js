fc4d71f639c3075e2e3c596afae27a40f6699494
Sential <sentialx@gmail.com>
2019-07-15 16:12:39 +0200

    chore: configuration update

--- example.js
+++ example.js
@@ -3,3 +3,7 @@ const mouseEvents = require("./dist/index");
 mouseEvents.default.on("mouse-up", data => {
   console.log(data);
 });
+
+mouseEvents.default.on("mouse-move", data => {
+  console.log(data);
+});
